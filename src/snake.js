0d923575d960d1739751d883dd3dc52d7d73f29e
trikitrok <trikitrok@gmail.com>
2014-07-12 17:48:15 +0200

    It can add two points

---
+++ src/snake.js
@@ -0,0 +1,5 @@
+var SnakeMath = {
+  addPoints: function(point1, point2) {
+    return [point1[0] + point2[0], point1[1] + point2[1]];
+  }
+};
---
+++ test/snakeSpec.js
@@ -0,0 +1,5 @@
+describe("SnakeMath", function() {
+  it("adds points", function() {
+    expect(SnakeMath.addPoints([1, 2], [2, 3])).toEqual([3, 5]);
+  });
+});
