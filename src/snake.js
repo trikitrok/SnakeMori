1ba178f54368ee99b2cb7ef1e6b552913594093e
trikitrok <trikitrok@gmail.com>
2014-07-13 02:40:13 +0200

    It transforms a point in game space to a rectangle in screen space

--- src/snake.js
+++ src/snake.js
@@ -1,21 +1,35 @@
 (function () {
   var root = this,
-    m, allong, snake;
+    m, allong, snake,
+    reduceToArray,
+    mapToArray;
 
   if (typeof require !== 'undefined') {
     m = require('mori');
     allong = require('allong.es');
   }
 
+  reduceToArray = m.comp(m.into_array, m.reduce);
+  mapToArray = m.comp(m.into_array, m.map);
+
   snake = {
     FunctionalModel: {
       addPoints: allong.es.variadic(function (points) {
-        return m.into_array(
-          m.reduce(
+        return reduceToArray(
             m.partial(m.map, m.sum),
-            points)
+            points
         );
-      })
+      }),
+
+      pointToScreenRectangle: function (point) {
+        var pointSize = 10;
+        return mapToArray(
+            function (num) {
+              return num * pointSize;
+            },
+            [point[0], point[1], 1, 1]
+        );
+      }
     }
   };
 
--- test/snakeMathSpec.js
+++ test/snakeMathSpec.js
@@ -4,7 +4,11 @@ describe("Snake's functional model", function () {
   var fns = snake.FunctionalModel;
 
   it("adds points", function () {
-    expect(fns.addPoints([1, 2], [2, 3], [-1,-2])).toEqual([2, 3]);
+    expect(fns.addPoints([1, 2], [2, 3], [-1, -2])).toEqual([2, 3]);
+  });
+
+  it("transforms a point in game space to a rectangle in screen space", function () {
+    expect(fns.pointToScreenRectangle([5, 10])).toEqual([50, 100, 10, 10]);
   });
 });
 
